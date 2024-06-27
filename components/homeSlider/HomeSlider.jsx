daa3b3011ba87b5ceede84e4314d168db9d9d5d4
Shamir Roy <shamirroy2020@gmail.com>
2024-06-27 18:18:07 +0600

    Third commit

--- components/homeSlider/HomeSlider.jsx
+++ components/homeSlider/HomeSlider.jsx
@@ -2,6 +2,9 @@
 import Link from 'next/link'
 import Carousel from 'react-bootstrap/Carousel'
 import Image from 'next/image'
+
+import styles from './styles.module.css'
+
 const HomeSlider = () => {
     return (
         <>
@@ -12,13 +15,15 @@ const HomeSlider = () => {
                         className="d-block w-100"
                         src="/doctor1.jpg"
                         width='1000'
-                        height='450'
+                        height='400'
                         alt="First slide"
                         style={{objectFit:"cover"}}
                     />
                     <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <div className={styles.sliderCaption}>
+                            <h3>Best doctors available. Book an appointment and get your prescription</h3>
+                            <Link href='/'><button className='btn btn-primary'>Book An Appointment</button></Link>
+                        </div>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -27,13 +32,15 @@ const HomeSlider = () => {
                         className="d-block w-100"
                         src="/doctor2.avif"
                         width='1000'
-                        height='450'
+                        height='400'
                         alt="First slide"
                         style={{objectFit:"cover"}}
                     />
                     <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className={styles.sliderCaption}>
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -42,15 +49,15 @@ const HomeSlider = () => {
                         className="d-block w-100"
                         src="/doctor3.avif"
                         width='1000'
-                        height='450'
+                        height='400'
                         alt="First slide"
                         style={{objectFit:"cover"}}
                     />
                     <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
+                    <div className={styles.sliderCaption}>
+                            <h5>Second slide label</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
